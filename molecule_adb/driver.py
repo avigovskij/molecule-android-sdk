271652a2a0ab062e24c74ce05f8615ab3b7ee00f
Anton Vigovskij <70585932+avigovskij@users.noreply.github.com>
2025-12-06 18:43:42 +0300

    Update: added stable version of termux, python3, openssh installation

--- molecule_adb/driver.py
+++ molecule_adb/driver.py
@@ -1,4 +1,5 @@
 import os
+import yaml
 
 from molecule.api import Driver
 
@@ -26,11 +27,54 @@ class ADBMoleculeDriver(Driver):
     def default_ssh_connection_options(self):
         return self._get_ssh_connection_options()
 
-    def login_cmd_template():
-        pass
+    def load_instances_data(self):
+        data = []
+        try:
+            with open(self.instance_config, 'r') as instance_data:
+                data = yaml.safe_load(instance_data)
+        except FileNotFoundError:
+            pass
+
+        return data
+
+    @property
+    def login_cmd_template(self) -> str:  # pragma: no cover
+        return """ssh -i {private_ssh_key_file} {ansible_user}@{ansible_host} -p {ansible_port} -o StrictHostKeyChecking=no"""
+
+    @property
+    def default_ssh_connection_options(self) -> list[str]:  # pragma: no cover
+        """SSH client options and returns a list.
+
+        :returns: list
+        """
+        return [
+            "StrictHostKeyChecking=no"
+        ]
+
+    def ansible_connection_options(
+        self,
+        instance_name: str,
+    ):
+        instances = self.load_instances_data()
+        instance_data = {}
+        for instance in instances:
+            if instance['name'] == instance_name:
+                instance_data = instance
+                break
+
+        return instance_data
 
     def login_options(self, instance_name):
-        return super().login_options(instance_name)
+        instances = self.load_instances_data()
+        instance_data = {}
+        for instance in instances:
+            if instance['name'] == instance_name:
+                instance_data = instance
+                break
+        else:
+            raise Exception(f'Config for instance named "{instance_name}" not found!')
+
+        return instance_data
 
     def sanity_checks(self):
         # TODO: Add implement of android SDK availablility
